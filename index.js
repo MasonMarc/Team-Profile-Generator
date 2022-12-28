5063dee441f714c8775f7a0e791075700c1eb834
MasonMarc <masonmarcantel@gmail.com>
2022-12-28 15:11:31 -0500

    made a selector for employee type

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
+const inquirer = require('inquirer');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const inquirer = require('inquirer');
+
+// choose what type of employee
+let empChoice = () => {
+const prompt = inquirer.createPromptModule();
+prompt([
+  {
+    type: 'list',
+    message: 'Please Select Employee type',
+    choices: ['Manager', 'Engineer', 'Intern'],
+    name: 'emp'
+  }])
+  .then((answer) => {
+    if (answer.emp === 'Manager'){
+        makeManger();
+    }
+    if (answer.emp === 'Engineer'){
+        makeEngineer();
+    }
+    if (answer.emp === 'Intern'){
+        makeIntern();
+    }
+})};
+
+// make Manager
+const makeManger = () => {
+    console.log('here is a manager');
+}
+// make Engineer
+const makeEngineer = () => {
+    console.log('here is a engineer');
+}
+// make Intern
+const makeIntern = () => {
+    console.log('here is a intern');
+}
 
 
 
+// at the end to add more eployees or stop program
+const addMore = () => {
+
+    empChoice();
+}
+
+empChoice();
