39ef6a8e79085bd6d6469d305d478f1227c78ae0
MasonMarc <masonmarcantel@gmail.com>
2022-12-29 18:34:55 -0500

    made first test for jest

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,5 +1,26 @@
 const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
+    describe("getName method", () => {
+        it("returns name", () => {
+            const employee = new Employee('Mason', 56, 'mason@masonmail.com');
+            employee.getName();
+            expect(employee.name).toBe('Mason');
+        })
+    });
+    describe("getId method", () => {
+        it("returns id", () => {
+            const employee = new Employee('Mason', 56, 'mason@masonmail.com');
+            employee.getId();
+            expect(employee.id).toBe(56);
+        })
+    });
+    describe("getEmail method", () => {
+        it("returns email", () => {
+            const employee = new Employee('Mason', 56, 'mason@masonmail.com');
+            employee.getEmail();
+            expect(employee.email).toBe('mason@masonmail.com');
+        })
+    });
 
 });
